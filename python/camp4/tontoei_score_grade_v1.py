d7bf93c39d27c16c9962fc37d068179a7373b3f7
LAPTOP-FOEFS48L\Cartoon Doppio <kornpassorn.h@doppio-tech.com>
2021-06-14 18:16:38 +0700

    tontoei score grade v1

---
+++ python/camp4/tontoei_score_grade_v1.py
@@ -0,0 +1,15 @@
+name = 'tontoei'
+score = 85
+x = str(score)
+g = ' grade'
+
+if score >=80 :
+	print(name + ' eual to ' + x + g + ' =A')
+elif score >=70 and score <80:
+	print(name + ' eual to ' + x + g + ' =B')
+elif score >=60 and score <70:
+	print(name + ' eual to ' + x + g + ' =C')
+elif score >=50 and score <50:
+	print(name + ' eual to ' + x + g + ' =D')
+else:
+	print(name + ' eual to ' + x + g + ' =F')
