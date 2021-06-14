ed519ac56d6beabc7d8ff5e7cd0a15083997da1a
2032539 <lalipat.k@doppio-tech.com>
2021-06-14 17:47:55 +0700

    Yean_student_grading_1

---
+++ python/camp4/Yean_student_grading_1.py
@@ -0,0 +1,16 @@
+x = 49
+if x >= 80 :
+    print("score = ",x,"Grade = A")
+
+if x >= 70 and  x < 80 :
+    print("score = ",x,"Grade = B")
+
+if x >= 60 and x < 70 :
+    print("score = ",x,"Grade = C")
+
+if x >= 50  and x < 60 :
+    print("score = ",x,"Grade = D")
+
+if x < 50 :
+    print("score = ",x,"Grade = F")  
+    
