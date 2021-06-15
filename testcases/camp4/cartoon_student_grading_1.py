c4f18e9afb228b29a6d11cc6b02f4a9fa1c35084
cartoonto <rungtiwa.k@doppio-tech.com>
2021-06-15 13:26:52 +0700

    python101
    python assignment 1 ka

---
+++ testcases/camp4/cartoon_student_grading_1.py
@@ -0,0 +1,11 @@
+score = 89
+if (score >= 80):
+    print('Score =',score,'Grade = A')
+elif (score >=70 and score < 80):
+    print('Score =',score,'Grade = B')
+elif (score >=60 and score < 70):
+    print('Score =',score,'Grade = C')
+elif (score >=50 and score < 60):
+    print('Score =',score,'Grade = D')
+elif (score < 50):
+    print('Score =',score,'Grade = F')
