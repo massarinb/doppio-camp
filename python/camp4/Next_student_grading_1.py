d5e1eb59574f5bc99fd8b7480f0217373298b475
NextDoppio <kritsada.p@doppio-tech.com>
2021-06-15 13:17:29 +0700

    grading score with 0-100 score and No decimals

--- python/camp4/Next_student_grading_1.py
+++ python/camp4/Next_student_grading_1.py
@@ -1,18 +1,26 @@
-x = 71
+x =  100.5
 # x= Score of Student.
-if x >= 80 :
+z = int(x) 
+y = float(x)
+	
+
+if x >= 80 and x <= 100 and x == z :
 	print("Score = ",x,",Grade = A") 
 
-elif x >= 70 and x < 80 :
+elif x >= 70 and x < 80 and x == z :
 	print("Score = ",x,",Grade = B") 
 
-elif x >= 60 and x < 70 :
+elif x >= 60 and x < 70 and x == z :
 	print("Score = ",x,",Grade = C") 
 
-elif x >= 50 and x < 60 :
+elif x >= 50 and x < 60 and x == z :
 	print("Score = ",x,",Grade = D")  
 
-elif x < 50 :
+elif x < 50 and x >= 0 and x == z :
 	print("Score = ",x,",Grade = F") 
 
-	
+elif x == y and x != z :
+	print("error number cannot be decimals") 
+
+else :
+	print("error score should be between 0 - 100 ")
